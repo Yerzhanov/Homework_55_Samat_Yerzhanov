042b4b8d493d2ebbc7386ca1697c8616fc170488
Yerzhanov <dearhauer@gmail.com>
2023-02-17 13:23:01 +0600

    Добавлено отображение статуса

---
+++ todo/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import TextChoices
+
+
+class StatusChoice(TextChoices):
+    ACTIVE = 'ACTIVE', 'Активна'
+    NOT_ACTIVE = 'NOT_ACTIVE', 'Неактивна'
+
+
+class Todolist(models.Model):
+    status = models.CharField(max_length=20, verbose_name='Статус', choices=StatusChoice.choices, default=StatusChoice.ACTIVE)
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
