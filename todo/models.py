7d6acca28cc97cb7b34da78452de59dd3c46a49c
Yerzhanov <dearhauer@gmail.com>
2023-02-24 14:04:59 +0600

    Добавлены фикстуры, новые миграции с полями удаления данных

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import TextChoices
+from django.utils import timezone
 
 
 class StatusChoice(TextChoices):
@@ -14,10 +15,17 @@ class Todolist(models.Model):
     memo = models.TextField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField(null=True, blank=True)
+    is_deleted = models.BooleanField(verbose_name='удалено', null=False, default=False)
+    date_deleted = models.DateTimeField(null=True, blank=True, default=None)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.date_deleted = timezone.now()
+        self.save()
 
 
